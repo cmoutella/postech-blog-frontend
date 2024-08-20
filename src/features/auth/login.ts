f095ca780684ad4db87d59c2cdf3ca3a89ef04ad
Carolina Moutella <ola@carolinamoutella.com>
2024-08-20 19:26:57 -0300

    chore: fix .env.example

--- src/features/auth/login.ts
+++ src/features/auth/login.ts
@@ -6,23 +6,35 @@ export const authLogin: (
   password: string
 ) => Promise<TeacherAuth | undefined> = async (username, password) => {
   // devemos esconder a senha na request e passar aplicando o SECRET?
-  const baseUrl = process.env.BASE_URL;
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  const loginUrl = `${baseUrl}/users/login`;
+
+  console.log("loginUrl", loginUrl);
 
   const credentials = { username: username, password: password };
 
   try {
-    const auth = await fetch(`${baseUrl}/users/login`, {
+    console.log("im trying");
+    debugger;
+    const auth = await fetch(loginUrl, {
       body: JSON.stringify(credentials),
-    }).then((res) => res.json());
+    });
 
-    if (auth.error) {
+    console.log("promise", auth);
+
+    const res = await auth.json();
+
+    console.log("res", res);
+
+    if (res.error) {
       throw Error("Não foi possivel completar atualizar suas credenciais");
     }
 
-    const { data } = auth as SuccessResponse<TeacherAuth>;
+    const { data } = res as SuccessResponse<TeacherAuth>;
 
     return data;
   } catch (err) {
+    console.log("deu ruim");
     return undefined;
   }
 };

77da2e62693762b883b051d0e735e7f8b71ce6e1
Carolina Moutella <ola@carolinamoutella.com>
2024-09-25 09:58:00 -0300

    fix: private layout and button variants

--- src/ui/components/admButtons/index.tsx
+++ src/ui/components/admButtons/index.tsx
@@ -1,17 +1,19 @@
-import { Button } from "@/components/ui/button"; 
+import { Button } from "@/components/ui/button";
 import { useSessionContext } from "@/providers/AuthProvider";
 
 export default function AdmButtons() {
   const { user } = useSessionContext();
   return (
-    <header className="flex justify-between items-center p-4 m-5">
+    <header className="container flex justify-between items-center mb-4 gap-10">
       <div>
-        <span className="text-xl font-semibold">Olá Prof. {user?.username}</span>
+        <span className="text-xl font-semibold">
+          Olá Prof. {user?.username}
+        </span>
       </div>
       <div className="flex gap-4">
-        <Button className="bg-zinc-800 text-white">Editar meus dados</Button>
-        <Button className="bg-fiap text-white">
-            <a href="/admin/post/create">Novo Post</a>
+        <Button variant="secondary">Editar meus dados</Button>
+        <Button variant="default">
+          <a href="/admin/post/create">Novo Post</a>
         </Button>
       </div>
     </header>
--- src/ui/templates/PrivateBasePage/index.tsx
+++ src/ui/templates/PrivateBasePage/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 import AdmButtons from "@/ui/components/admButtons";
+import Navbar from "@/ui/components/navbar";
 import type { ReactNode } from "react";
 
 export interface HeaderProps {
@@ -13,7 +14,8 @@ const placeholderId = "private-header-placeholder";
 export function PrivateBasePage({ children }: Omit<HeaderProps, "title">) {
   return (
     <div className="antialiased">
-      <main className="h-auto p-4">
+      <Navbar />
+      <main className="h-auto p-4 pt-10">
         <AdmButtons />
         <div className="container mx-auto max-w-6xl">{children}</div>
       </main>
